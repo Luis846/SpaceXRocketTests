49788eeb93685a1c5a25dcb4ea4b4047fb2d5f2d
Luis <luis.rivera846@gmail.com>
2020-01-11 12:44:37 -0500

    added functionality

---
+++ public/precache-manifest.539800765f90380284de296e3b072de6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668807a2d4bef2e0d7bccbd157486a6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c46c696d0663d27ca9e4",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "966185e3525ddeacb4db",
+    "url": "/static/js/2.55737d47.chunk.js"
+  },
+  {
+    "revision": "786089ef6a78275eeee7a67aba60641a",
+    "url": "/static/js/2.55737d47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c46c696d0663d27ca9e4",
+    "url": "/static/js/main.99caf24a.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "c5160290c09c82ba508abb49f46c9872",
+    "url": "/static/media/logo.c5160290.png"
+  }
+]);
